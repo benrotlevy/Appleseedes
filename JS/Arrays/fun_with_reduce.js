21c5e5e374d2a1eaf0efbef97bbf8162c698a233
benrotlevy <benjamin@rotlevy.com>
2022-04-17 11:24:41 +0300

    daily asighnment-2

---
+++ JS/Arrays/fun_with_reduce.js
@@ -0,0 +1,29 @@
+const numbers = [10,40,30,5,25];
+
+const max = (arr) => {
+    return arr.reduce((max, current) => {
+        if(max < current) return current;
+        return max;
+    })
+}
+
+// console.log(max(numbers));
+
+const sumOfEven = (arr) => {
+    return arr.reduce((sum, current) => {
+        if(sum % 2 === 1) sum = 0;
+        if(current % 2 === 0) return sum += current;
+        return sum;
+    })
+}
+
+// console.log(sumOfEven(numbers));
+
+const average = (arr) => {
+    return arr.reduce((total, current) => {
+        if(arr.indexOf(current)+1 === arr.length) return (total + current) / arr.length;
+        return (total + current);
+    })
+}
+
+// console.log(average(numbers));
